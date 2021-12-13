9f82010e01b3c7e60524721404fca84363fe7515
Louis Lacombe <lfmm.lacombe@gmail.com>
2021-12-13 14:12:37 +0100

    change contract

--- contracts/BocconiToken.sol
+++ contracts/BocconiToken.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/2a09e50d09a6795b4bfdfae0075bdd5ca88e4d97/contracts/token/ERC20/ERC20.sol";
+
 contract BocconiToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("BocconiToken", "BT") {
         _mint(msg.sender, initialSupply);
--- scripts/1_deploy_token.py
+++ scripts/1_deploy_token.py
@@ -2,10 +2,21 @@ from brownie import BocconiToken
 from scripts.helpful_scripts import get_account
 from web3 import Web3
 
-initial_supply = Web3.toWei(1000, "ether")
+initial_supply = Web3.toWei(10, "ether")
 
 
 def main():
     account = get_account()
     our_token = BocconiToken.deploy(initial_supply, {"from": account})
-    print(our_token.name())
+    # print(our_token.balanceOf(account))
+
+    print(our_token.balanceOf(account))
+    print(our_token.balanceOf(get_account(index=1)))
+
+    our_token.increaseAllowance(get_account(index=1), 20)
+    print(our_token.balanceOf(account))
+    print(our_token.balanceOf(get_account(index=1)))
+
+    our_token.transferFrom(get_account(index=1), account, 1)
+    print(our_token.balanceOf(account))
+    print(our_token.balanceOf(get_account(index=1)))
